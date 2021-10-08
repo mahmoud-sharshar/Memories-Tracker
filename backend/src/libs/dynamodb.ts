341be60165aac682582f0f4e8cc4d282aa54340f
Mahmoud Sharshar <mahmoudsharshar3@gmail.com>
2021-10-08 11:16:35 +0200

    Add cors middleware to function handlers

---
+++ backend/src/libs/dynamodb.ts
@@ -0,0 +1,16 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+export const createDynamoDBClient = () => {
+  if (process.env.IS_OFFLINE) {
+    console.log('Creating a local DynamoDB instance')
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+
+  return new XAWS.DynamoDB.DocumentClient()
+}
